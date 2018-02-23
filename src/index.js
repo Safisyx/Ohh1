aadce18e51f04be4a92b77897561125743e43f92
Safisyx <safisyx@email.com>
2018-02-23 12:44:36 +0100

    Create board reducer

--- src/index.js
+++ src/index.js
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux'
+import store from './store'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//ReactDOM.render(<App />, document.getElementById('root'));
+//registerServiceWorker();
+
+// src/index.js
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
 registerServiceWorker();
---
+++ src/reducers/board.js
@@ -0,0 +1,9 @@
+//scr/reducers/board.js
+
+// export default function(state = [], action = {}) {
+//   return state
+// }
+
+export default (state = [], { type, payload } = {}) => {
+  return state
+}
