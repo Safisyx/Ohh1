53cc23df7aa75a9dd3540de0a1191735cf6e1e93
Safisyx <safisyx@email.com>
2018-02-23 11:48:39 +0100

    Setup App testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+// it('renders without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<App />, div);
+//   ReactDOM.unmountComponentAtNode(div);
+// });
+
+import React from 'react'
+import { shallow } from 'enzyme'
+import App from './App'
+
+describe('<App />', () => {
+  const app = shallow(<App />)
+
+  it('wraps everything in a div tag', () => {
+    expect(app).toHaveTagName('div')
+    expect(app).toHaveClassName('App')
+  })
+
+  it('contains a title', () => {
+    expect(app.find('h1.title')).toHaveText('0hh1')
+  })
+})
